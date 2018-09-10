81c316b8e7d836ff90c85944e984bca469784403
Mahsa Sirous <msirouss@gmail.com>
2018-09-10 22:30:15 +0430

    DigiKala test.

---
+++ DigiKala.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Msirous
+ * Date: 9/10/18
+ * Time: 12:19 PM
+ */
+
+//echo "test";
+
+
+// Create DOM from URL or file
+include ("simple_html_dom.php");
+$html = file_get_html("https://www.digikala.com/product/dkp-90825");
+
+// Show Me all of Text in Digikala.
+echo $html->plaintext;
