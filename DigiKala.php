47a67fe0074f58197de23b1b7e25935587a8848f
Mahsa Sirous <msirouss@gmail.com>
2018-09-10 23:39:40 +0430

    test that show me page titel on TOP of the DigiKala Site.

--- DigiKala.php
+++ DigiKala.php
@@ -14,4 +14,8 @@ include ("simple_html_dom.php");
 $html = file_get_html("https://www.digikala.com/product/dkp-90825");
 
 // Show Me all of Text in Digikala.
-echo $html->plaintext;
+//echo $html->plaintext;
+
+// Show Title On top Of The site.
+$title = $html->find("title",0)->innertext;
+echo "<html dir = rtl>".$title . "</html>";
